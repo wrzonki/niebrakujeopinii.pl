ba338e6c98d1dacba1b99d6809defae0552c07d5
wrzonki <wrzonki@gmail.com>
2022-06-30 20:56:56 +0200

    first commit 🚀

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<div>
+  <img src="tvp.jpg" alt="background image" />
+  <h2>Strona w budowie</h2>
+</div>
+
+<style>
+  :global(body) {
+    background-color: #eee;
+    margin: 0 auto;
+  }
+  div {
+    position: absolute;
+    margin-left: calc(50% - 640px);
+  }
+  h2 {
+    color: white;
+    text-transform: uppercase;
+    z-index: 10;
+    position: absolute;
+    top:536px;
+    left: 270px;
+    font-size: 40px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
+  }
+</style>
